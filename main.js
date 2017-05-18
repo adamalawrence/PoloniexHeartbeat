346eb7ca286f0b0301ac321930e0aae52582b697
adamadlawrence <adam.ad.lawrence@gmail.com>
2017-05-18 13:16:34 -0700

    updatable graph

--- main.js
+++ main.js
@@ -1,20 +1,6 @@
 var Poloniex = require('poloniex.js');
 var jsnx = require('jsnetworkx');
 
-var polo = new Poloniex();
-
-
-var G = new jsnx.Graph();
-G.addNode([1,2], {data: 'foo'});
-console.log(G.node.get([1,2]).data); // because [1,2].toString() === "1, 2"
-
-G.addNodesFrom([2, 3]);
-G.addNodesFrom(new Set('foo', 'bar'));
-var H = jsnx.completeGraph(10);
-G.addNodesFrom(H);
-console.log(G);
-
-
 /*
 let count = 0;
 setInterval(function () {
